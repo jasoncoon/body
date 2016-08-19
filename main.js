557d35ef5a12ad8978eb629d0f4d5917c529b319
Jason Coon <jasoncoon@users.noreply.github.com>
2016-08-18 20:41:25 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,287 @@
+var authtoken;
+
+var entries = [];
+var weightchartdata = [];
+var bodyfatchartdata = [];
+var musclechartdata = [];
+var waterchartdata = [];
+
+$("#email").val(localStorage.getItem("email"));
+$("#password").val(localStorage.getItem("password"));
+
+$("#signinform").submit(function(event) {
+  event.preventDefault();
+  signin();
+});
+
+$("#signoutbutton").click(function() {
+  entries = [];
+  weightchartdata = [];
+  bodyfatchartdata = [];
+  musclechartdata = [];
+  waterchartdata = [];
+
+  $("#signinform").show();
+
+  // hide the controls
+  $("#toolbar").hide();
+  $("#tablecontainer").hide();
+  $("#weightchart").hide();
+  $("#bodyfatchart").hide();
+  $("#musclechart").hide();
+  $("#waterchart").hide();
+});
+
+$("#getNewEntriesButton").click(function() {
+  getNewEntries();
+});
+
+function signin() {
+  var email = $("#email").val();
+  var password = $("#password").val();
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+  password = sha256(password);
+  var formData = {
+    email: email,
+    password: password
+  };
+
+  $.ajax({
+      url: "https://api.weightgurus.com/v2/user/login",
+      type: "POST",
+      headers: {
+        Accept: "application/json"
+      },
+      data: formData
+    })
+    .done(function(data) {
+      authtoken = data.auth_token;
+      $("#signinform").hide();
+      getNewEntries();
+    });
+}
+
+function compareEntriesByTimestampDesc(a, b) {
+  var at = 0;
+  var bt = 0;
+
+  if (a != null && a.timestamp != null)
+    at = a.timestamp;
+
+  if (b != null && b.timestamp != null)
+    bt = b.timestamp;
+
+  return bt - at;
+}
+
+function removeEntry(array, item) {
+  // find the entry in the array
+  var entry = array.find(function(element, index, array) {
+    return element[0] = item.timestamp;
+  }, item);
+
+  // remove the entry from the array
+  if (entry != null) {
+    var entryIndex = array.indexOf(entry);
+    array.splice(entryIndex, 1);
+  }
+}
+
+function getNewEntries() {
+  $("#table > tbody").empty();
+
+  var start = 1000000000000;
+
+  // if (entries.length > 0)
+  //   start = entries[0].timestamp + 1000;
+
+  var formData = {
+    auth_token: authtoken,
+    start: start
+  };
+
+  $.ajax({
+      url: "https://api.weightgurus.com/v2/entry/list",
+      type: "POST",
+      headers: {
+        Accept: "application/json"
+      },
+      data: formData
+    })
+    .done(function(data) {
+      entries = data;
+      weightchartdata = [];
+      bodyfatchartdata = [];
+      musclechartdata = [];
+      waterchartdata = [];
+
+      // sort the entries newest to oldes
+      entries.sort(compareEntriesByTimestampDesc);
+
+      entries.forEach(function(item, index) {
+        if (item.op != "create")
+          return;
+
+        var timestamp = item.timestamp;
+        var date = new Date(timestamp);
+
+        // add the entry to the chart
+        if (item.weight != null)
+          weightchartdata.push([
+            timestamp,
+            item.weight
+          ]);
+
+        if (item.body_fat != null)
+          bodyfatchartdata.push([
+            timestamp,
+            item.body_fat
+          ]);
+
+        if (item.muscle_mass != null)
+          musclechartdata.push([
+            timestamp,
+            item.muscle_mass
+          ]);
+
+        if (item.water != null)
+          waterchartdata.push([
+            timestamp,
+            item.water
+          ]);
+
+        // add the entry to the table
+        $("#table > tbody:last-child")
+          .append($('<tr id="' + item.timestamp + '">')
+            .append($('<td>')
+              .text(date.toLocaleDateString())
+            )
+            .append($('<td>')
+              .text(item.weight)
+            )
+            .append($('<td>')
+              .text(item.body_fat)
+            )
+            .append($('<td>')
+              .text(item.water)
+            )
+            .append($('<td>')
+              .text(item.muscle_mass)
+            )
+            .append($('<td>')
+              .text(item.type)
+            )
+          );
+      });
+
+      // remove any entries that are marked for deletion
+      entries.forEach(function(item, index) {
+        if (item.op != "delete")
+          return;
+
+        // remove the entry from the table
+        // $("#" + item.timestamp).addClass("danger");
+        $("#" + item.timestamp).remove();
+
+        removeEntry(weightchartdata, item);
+        removeEntry(bodyfatchartdata, item);
+        removeEntry(musclechartdata, item);
+        removeEntry(waterchartdata, item);
+      });
+
+      // show the controls
+      $("#toolbar").show();
+      $("#tablecontainer").show();
+      $("#weightchart").show();
+      $("#bodyfatchart").show();
+      $("#musclechart").show();
+      $("#waterchart").show();
+
+      // load the charts
+      $('#weightchart').highcharts({
+        title: {
+          text: 'Weight'
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        yAxis: {
+          title: {
+            text: 'Pounds'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          type: 'line',
+          data: weightchartdata
+        }]
+      });
+
+      $('#bodyfatchart').highcharts({
+        title: {
+          text: 'Body Fat'
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        yAxis: {
+          title: {
+            text: '%'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          type: 'line',
+          data: bodyfatchartdata
+        }]
+      });
+
+      $('#musclechart').highcharts({
+        title: {
+          text: 'Muscle Mass'
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        yAxis: {
+          title: {
+            text: '%'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          type: 'line',
+          data: musclechartdata
+        }]
+      });
+
+      $('#waterchart').highcharts({
+        title: {
+          text: 'Water'
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        yAxis: {
+          title: {
+            text: '%'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          type: 'line',
+          data: waterchartdata
+        }]
+      });
+
+    });
+}
